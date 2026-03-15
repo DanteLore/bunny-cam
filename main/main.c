cc6e2ea4fa63c7f29cbcff8a4df84e5f6742b72c
DanteLore <dan.taylor@live.co.uk>
2026-03-15 21:23:42 +0000

    Hostname fix

--- main/main.c
+++ main/main.c
@@ -3,6 +3,7 @@
 #include "esp_err.h"
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_netif.h"
 #include "esp_wifi.h"
 #include "nvs_flash.h"
 #include "freertos/task.h"
@@ -39,7 +40,8 @@ static void wifi_init(void)
 
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
+    esp_netif_t *netif = esp_netif_create_default_wifi_sta();
+    esp_netif_set_hostname(netif, "bunny-cam");
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
